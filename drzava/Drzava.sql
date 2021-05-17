67e0a9bb8e780c878b4a83b2be7235e51093e018
igorvucko <igorhefner@gmail.com>
2021-05-17 15:25:01 +0200

    Create Drzava.sql

---
+++ drzava/Drzava.sql
@@ -0,0 +1,54 @@
+drop database if exists drzava;
+create database drzava;
+use drzava;
+
+create table zupanija(  
+sifra int primary key auto_increment,
+naziv varchar(50),
+zupan int 
+);
+
+create table opcina( 
+sifra int primary key auto_increment,
+zupanija int,
+naziv varchar(50)
+);
+
+create table zupan( 
+sifra int primary key auto_increment,
+ime varchar(50),
+prezime varchar(50)
+);
+
+create table mjesto( 
+sifra int primary key auto_increment,
+opcina int,
+naziv varchar(50)
+);
+
+alter table opcina add foreign key (zupanija) references zupanija(sifra);
+alter table mjesto add foreign key (opcina) references opcina(sifra);
+alter table zupanija add foreign key (zupan) references zupan(sifra);
+
+select * from zupanija;
+
+insert into zupanija(naziv) values ('Osjeèko-baranjska'),
+('Vukovarsko-Srijemska'),('Virovitièko-podravska');
+
+select * from opcina;
+
+insert into opcina(zupanija,naziv) values(1,'Osijek'),(2,'Bertiæi'),
+(3,'Virovitica'),(2,'Vukovar'),(1,'Sarvaš'),(2,'Podgajci');
+
+select * from zupan;
+
+insert into zupan(ime,prezime)
+values ('Pero','Žderiæ'),('Andrej','Plenki'),('Josip','Borki');
+
+select * from mjesto;
+
+insert into mjesto(opcina,naziv)
+values(1,'Višnjevac'),(2,'Prigajci'),(3,'Mirkovci'),(1,'Podravlje'),
+(2,'Stupnik'),(3,'Èuturevci'),(6,'Tvrðavica'),(5,'Sevljanci'),
+(6,'Kitišanci'),(4,'Mirkovci'),(4,'Viljkovci'),(5,'Šiškovci');
+
